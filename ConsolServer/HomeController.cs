4a93ce003917e754f6a68afc38d506256a03949f
nto4 <mehmet94nto@gmail.com>
2019-07-12 16:11:56 +0300

    selfhost eklendi

---
+++ ConsolServer/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using OgrenciMVC.Models;
+using OgrenciMVC.Repository;
+
+namespace ConsolServer
+{
+   
+        public class HomeController : ApiController
+        {
+             IRepostory<OgrModel> _Repostory = new OgrRepostory();
+            // GET: api/Home
+           // readonly IRepostory<OgrModel> _Repostory;
+            //public HomeController(IRepostory<OgrModel> ogrenci)
+            //{
+            //    //_Repostory = new OgrRepostory();
+
+            //    _Repostory = ogrenci;
+            //}
+            public IEnumerable<OgrModel> Get()
+            {
+                return _Repostory.GetAll();
+            }
+
+            // GET: api/Home/5
+            public OgrModel Get(int id)
+            {
+                return _Repostory.GetbyId(id);
+            }
+
+            // POST: api/Home
+            public bool Post([FromBody]OgrModel ogrenci)
+            {
+                if (ModelState.IsValid)
+                {
+                    _Repostory.Insert(ogrenci);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);// how return status cood with booleans 
+
+                }
+                /*
+                try
+                {
+                    _Repostory.Insert(ogrenci);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+                    return false;
+                }
+                */
+
+
+            }
+
+            // PUT: api/Home/5
+            public void Put([FromBody]OgrModel ogrenci)
+            {
+                _Repostory.Edit(ogrenci);
+            }
+
+            // DELETE: api/Home/5
+            public void Delete(int id)
+            {
+                _Repostory.Delete(id);
+            }
+        }
+    }
+
+    
--- ConsolServer/Program.cs
+++ ConsolServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Http.SelfHost;
 
 namespace ConsolServer
@@ -11,8 +12,19 @@ namespace ConsolServer
     {
         static void Main(string[] args)
         {
-
             var config = new HttpSelfHostConfiguration("http://localhost:1234");
+            config.Routes.MapHttpRoute("default",
+                                        "api/{controller}/{id}",
+                                        defaults: new { id = RouteParameter.Optional });
+                                        //new { controller = "Home", id = RouteParameter.Optional });
+
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+
+            Console.WriteLine("Web API Server has started at http://localhost:1234");
+            Console.ReadLine();
+            /*var config = new HttpSelfHostConfiguration("http://localhost:1234");
 
             var server = new HttpSelfHostServer(config, new MyWebAPIMessageHandler());
             var task = server.OpenAsync();
@@ -20,6 +32,7 @@ namespace ConsolServer
 
             Console.WriteLine("Web API Server has started at http://localhost:1234");
             Console.ReadLine();
+            */
         }
     }
 }
